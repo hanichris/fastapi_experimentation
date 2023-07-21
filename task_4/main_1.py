b285bce02d73f68c213a4008fa3b32e20d2b0f05
hanichris <hanichris71@gmail.com>
2023-07-21 20:17:47 +0000

    Perform numeric validation through the use of keywords like `ge`, `gt`, `le` and `lt`

---
+++ task_4/main_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from fastapi import FastAPI, Query, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/items/{item_id}')
+async def read_items(
+        *,
+        item_id: Annotated[int, Path(title='The ID of the item to get', ge=0, le=1000)],
+        q: str | None = None,
+        size: Annotated[float, Query(gt=0, lt=10.5)]):
+    results = {'item_id': item_id}
+    if q:
+        results.update({'q': q})
+    return results
