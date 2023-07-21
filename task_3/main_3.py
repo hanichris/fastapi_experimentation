7b598dee5ecce3a6a19222a53c90a267c27ee512
hanichris <hanichris71@gmail.com>
2023-07-21 19:56:07 +0000

    Provide multiple values for a query parameter

---
+++ task_3/main_3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Providing additional information and validation for parameters.
+
+`Annotated` - Enables addition of metadata to the parameters.
+"""
+from fastapi import FastAPI, Query
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/items/')
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
+    if q:
+        results.update({'q': q})
+    return results
+
