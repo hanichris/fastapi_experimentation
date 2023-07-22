e9207bee641ecb9ce978cc47c81793493f7c4830
hanichris <hanichris71@gmail.com>
2023-07-22 20:59:35 +0000

    using query parameter as well without the need to call `Query`

---
+++ task_5/main_3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Treating singular values as a part of a Request Body."""
+from typing import Annotated
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+app = FastAPI()
+
+@app.put('/items/{item_id}')
+async def update_item(
+        item_id: int,
+        user: User,
+        item: Item,
+        importance: Annotated[int, Body()],
+        q: str | None = None):
+    result = {'item_id': item_id, 'item': item, 'user': user, 'importance': importance}
+    if q:
+        result.update({'q': q})
+    return result
+
