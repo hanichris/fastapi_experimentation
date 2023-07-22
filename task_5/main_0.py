7cc5d4b87a4ada1f010b72ac874e9f1edccdbe06
hanichris <hanichris71@gmail.com>
2023-07-22 20:25:45 +0000

    Working with Path, Query and Request Body parameters. Possible to make the request body parameter optional through provision of a default value.

---
+++ task_5/main_0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Working with the Request Body having multiple parameters."""
+from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel
+
+from typing import Annotated
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put('/items/{item_id}')
+async def update_item(
+        item_id: Annotated[int, Path(title='The ID of the item to get', ge=0, le=1000)],
+        q: str | None = None,
+        item: Item | None = None):
+    results = {'item_id': item_id}
+    if q:
+        results.update({'q': q})
+    if item:
+        results.update({'item': item})
+    return results
+
