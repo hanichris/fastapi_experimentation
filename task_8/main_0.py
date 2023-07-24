5e55d1776fc0129dece87176d64e97cd5df99050
hanichris <hanichris71@gmail.com>
2023-07-24 20:44:07 +0000

    Use the `Cookie` parameter to read the cookie of a request.

---
+++ task_8/main_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from fastapi import Cookie, FastAPI
+
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/items/')
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {'ads_id': ads_id}
+
