8cbcb2d10cc1d940c2da192bbf77c7954e2fd716
hanichris <hanichris71@gmail.com>
2023-07-31 14:42:05 +0000

    Create a password context and create utility functions to verify a password, and to generate a hash of a password

---
+++ task_13/main_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from datetime import datetime, timedelta
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+#GLOBAL VARIABLES FOR CONFIGURATION
+SECRET_KEY = ""
+ALGORITHM = ""
+ACCESS_TOKEN_EXPIRE_MINUTES = 301
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
