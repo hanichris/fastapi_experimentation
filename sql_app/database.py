0d6041fd4485f9dcd4f4712c0ab2cb60337d0a50
hanichris <hanichris71@gmail.com>
2023-08-01 20:22:45 +0000

    Properly format the relative path to the sqlite file

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,12 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = 'sqlite://./sql_app.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///sql_app.db'
 
+# the connect_args parameter used is only when working with sqlite.
 engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 class Base(DeclarativeBase):
     pass
