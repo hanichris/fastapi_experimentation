70824acabc70d0efadd53fa5ed833b4d919db651
hanichris <hanichris71@gmail.com>
2023-08-02 01:12:34 +0000

    Create the path operation functions

--- sql_app/main.py
+++ sql_app/main.py
@@ -33,6 +33,27 @@ def read_users(
         limit: int = 100,
         db: Annotated[Session, Depends(get_db)]):
     users = crud.get_users(db, skip=skip, limit=limit)
-    print(type(users[0]))
     return users
 
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return db_user
+
+@app.post('/users/{user_id}/items/', response_model=schemas.Item)
+def create_item_for_user(
+        user_id: int,
+        item: schemas.ItemCreate,
+        db: Annotated[Session, Depends(get_db)]):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+@app.get('/items/', response_model=list[schemas.Item])
+def read_items(
+        *,
+        skip: int = 0,
+        limit: int = 100,
+        db: Annotated[Session, Depends(get_db)]):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
